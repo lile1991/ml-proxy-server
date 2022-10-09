a730133117bdf426eefb026b54e3a27d22444da2
lile <285051173@qq.com>
2022-10-09 14:21:49 +0800

    localAddress

--- src/main/java/io/ml/proxy/server/handler/http/proxy/HttpConnectToHostHandler.java
+++ src/main/java/io/ml/proxy/server/handler/http/proxy/HttpConnectToHostHandler.java
@@ -167,7 +167,10 @@ public class HttpConnectToHostHandler extends ChannelInboundHandlerAdapter {
 
         if(localAddress != null) {
             // Bind local net address
+            log.debug("remoteAddress={}", localAddress);
             bootstrap.remoteAddress(localAddress);
+            log.debug("localAddress={}", localAddress);
+            bootstrap.localAddress(localAddress);
         }
 
         return bootstrap.connect(httpRequestInfo.getRemoteHost(), httpRequestInfo.getRemotePort());
