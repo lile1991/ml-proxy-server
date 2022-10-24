7320945db67d0aa51f1e1b051fa962c343ca85f1
lile <285051173@qq.com>
2022-10-24 14:19:09 +0800

    代理配置

--- src/main/java/io/ml/proxy/utils/net/ProxyConfigList.java
+++ src/main/java/io/ml/proxy/utils/net/ProxyConfigList.java
@@ -15,11 +15,9 @@ import java.util.stream.Collectors;
 @Slf4j
 public class ProxyConfigList {
     public static final Map<String, List<Proxy>> PROXY_LIST_MAP = new HashMap<>();
-    private static final Random RANDOM = new Random();
 
     /**
      * @param path 类路径下的代理配置， 如 proxy/us.txt"
-     * @return
      */
     private static synchronized List<Proxy> initProxies(String path) {
         if(!PROXY_LIST_MAP.containsKey(path)) {
@@ -38,35 +36,55 @@ public class ProxyConfigList {
 
             if(proxyFile.exists()) {
                 try (InputStream resourceAsStream = new FileInputStream(proxyFile)) {
-                    List<String> proxyList = IOUtils.readLines(resourceAsStream, "UTF-8");
-                    List<Proxy> proxies = proxyList.stream().map(socks5Proxy -> {
-                        String[] split = socks5Proxy.split(":");
-                        // SOCKS5/HTTPS/HTTP
-                        String protocol = split[0];
-                        String hostname = split[1];
-                        int port = Integer.parseInt(split[2]);
-                        String username = "";
-                        String password = "";
-                        if(split.length > 3) {
-                            username = split[3];
-                        }
-                        if(split.length > 4) {
-                            password = split[4];
-                        }
-
-                        return new Proxy(ProxyProtocolEnum.valueOf(protocol), null, hostname, port, username, password);
-                    }).collect(Collectors.toList());
+                    List<String> socks5ProxyList = IOUtils.readLines(resourceAsStream, "UTF-8");
+                    List<Proxy> proxies = socks5ProxyList.stream()
+                            .map(ProxyConfigList::parseProxyStr)
+                            .collect(Collectors.toList());
                     PROXY_LIST_MAP.put(path, proxies);
                     return proxies;
                 } catch (IOException e) {
                     log.error("Initial proxy IP exception", e);
                     PROXY_LIST_MAP.put(path, Collections.emptyList());
                 }
-            } else {
-                log.warn("The file {} not exists!", proxyFile.getAbsolutePath());
             }
         }
         return Collections.emptyList();
+    }
+
+    public static Proxy parseProxyStr(String proxyStr) {
+        // HTTP:proxy-1:hl888888@74.91.26.90:40000
+        // 1. 先解协议
+        int idx = proxyStr.indexOf(":");
+        String protocol = proxyStr.substring(0, idx);
+        proxyStr = proxyStr.substring(idx + 1);
+
+        String username = "";
+        String password = "";
+
+        // 2. 解用户名密码
+        // proxy-1:hl888888@74.91.26.90:40000
+        // proxy-1:@74.91.26.90:40000
+        // 74.91.26.90:40000
+        int upIdx = proxyStr.indexOf("@");
+        if(upIdx != -1) {
+            String usernamePassword = proxyStr.substring(0, upIdx);
+            // 有用户名密码
+            String[] up = usernamePassword.split(":", -1);
+            username = up[0];
+            if(up.length > 1) {
+                password = up[1];
+            }
+
+            proxyStr = proxyStr.substring(upIdx + 1);
+        }
+
+        // 3. 解代理主机
+        // 74.91.26.90:40000
+        String[] split = proxyStr.split(":");
+        String hostname = split[0];
+        int port = Integer.parseInt(split[1]);
+
+        return new Proxy(ProxyProtocolEnum.valueOf(protocol), null, hostname, port, username, password);
     }
 
     /**
@@ -78,6 +96,6 @@ public class ProxyConfigList {
         if(proxies == null) {
             proxies = initProxies(path);
         }
-        return new ArrayList<>(proxies);
+        return proxies;
     }
 }
